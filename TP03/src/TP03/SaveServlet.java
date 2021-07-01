b71a0e4e1458411a2f9ceb4d0909ff1fb3de5ee4
M1ch43 <82531218+M1ch43@users.noreply.github.com>
2021-07-01 19:09:03 -0300

    Add files via upload

---
+++ TP03/src/TP03/SaveServlet.java
@@ -0,0 +1,42 @@
+//michael regis de almeida cb 1740334
+
+package TP03;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SaveServlet")
+public class SaveServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String title=request.getParameter("title");
+        String author=request.getParameter("author");
+        String sprice=request.getParameter("price");
+        float price=Float.parseFloat(sprice);
+
+        book e=new book();
+        e.setTitle(title);
+        e.setAuthor(author);
+        e.setPrice(price);
+
+		int status = bookDao.save(e);
+		if (status > 0) {
+
+			out.print("<p>Record saved successfully!</p>");
+			request.getRequestDispatcher("index.jsp").include(request, response);
+		} else {
+			out.println("Sorry! unable to save record");
+		}
+
+		out.close();
+	}
+}
